df4caf405b3f721620685a654a8a7c3e32df4f21
TPavZ <tmpavlic@gmail.com>
2022-01-11 09:00:50 -0600

    Added a view all songs from API, display in table.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import MusicTable from "./Components/MusicTable/MusicTable";
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs() {
+    let response = await axios.get("http://www.devcodecampmusiclibrary.com/api/music/");
+    setSongs(response.data)
+  }
+
   return (
     <div>
-      <MusicTable />
+      <MusicTable songs={songs} />
     </div>
   );
 }
